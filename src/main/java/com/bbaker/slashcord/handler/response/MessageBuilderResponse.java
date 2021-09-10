373078e8d41bba0a6ed6e4864841a27f6ed3a02b
ben baker <benbaker.com@gmail.com>
2021-09-09 19:51:05 -0500

    MessageBuilder actually works now.
    But poorly

--- src/main/java/com/bbaker/slashcord/handler/response/MessageBuilderResponse.java
+++ src/main/java/com/bbaker/slashcord/handler/response/MessageBuilderResponse.java
@@ -3,12 +3,14 @@ package com.bbaker.slashcord.handler.response;
 import java.util.function.BiConsumer;
 
 import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.MessageFlag;
 import org.javacord.api.interaction.SlashCommandInteraction;
 
 public class MessageBuilderResponse  implements BiConsumer<Object, SlashCommandInteraction>  {
 
     @Override
     public void accept(Object value, SlashCommandInteraction sci) {
+        sci.createImmediateResponder().setFlags(MessageFlag.EPHEMERAL).append("success").respond();
         MessageBuilder mb = (MessageBuilder) value;
         sci.getChannel().ifPresent(channel -> mb.send(channel));
     }
