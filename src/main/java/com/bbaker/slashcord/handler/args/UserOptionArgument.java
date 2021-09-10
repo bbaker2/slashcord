155220df39e665e63fbc634920bfc7539a56b7f9
ben baker <benbaker.com@gmail.com>
2021-09-10 12:02:56 -0500

    The user option checks the cache, then a nework call

--- src/main/java/com/bbaker/slashcord/handler/args/UserOptionArgument.java
+++ src/main/java/com/bbaker/slashcord/handler/args/UserOptionArgument.java
@@ -1,6 +1,10 @@
 package com.bbaker.slashcord.handler.args;
 
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
 import org.javacord.api.entity.user.User;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
 import org.javacord.api.interaction.SlashCommandInteractionOptionsProvider;
 
 public class UserOptionArgument extends AbstractOptionArgument<User> {
@@ -11,7 +15,30 @@ public class UserOptionArgument extends AbstractOptionArgument<User> {
 
     @Override
     protected User getValue(SlashCommandInteractionOptionsProvider interaction) {
-        return interaction.getOptionUserValueByName(name).orElse(null);
+        // check to see if the option exists at all
+        Optional<SlashCommandInteractionOption> unsafeOption = interaction.getOptionByName(name);
+        if(unsafeOption.isEmpty()) {
+            return null;
+        }
+
+        // If so, attempt to grab the user from the cache
+        SlashCommandInteractionOption userOption = unsafeOption.get();
+        Optional<User> cachedUser = userOption.getUserValue();
+
+        if(cachedUser.isPresent()) {
+            return cachedUser.get();
+        }
+
+        // If the user is not in the cache, perform a network call to retrieve the user info
+        Optional<CompletableFuture<User>> queriedUser = userOption.requestUserValue();
+
+        if(queriedUser.isPresent()) {
+            // perform the network call, return null if the network call fails
+            return queriedUser.get().exceptionally(e -> null).join();
+        }
+
+        // if we somehow failed to prepare a network call, return null
+        return null;
     }
 
 }
